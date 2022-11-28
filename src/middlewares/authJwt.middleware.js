6fada21fd69a11331b2dad0652af741b8fb42ddf
Sagheer Hussain Zardari <sagheerhzardari@gmail.com>
2022-11-28 15:40:08 +0500

    added jwtverify response error services

--- src/middlewares/authJwt.middleware.js
+++ src/middlewares/authJwt.middleware.js
@@ -1,11 +1,9 @@
 import Jwt from "jsonwebtoken";
-
+import ResponseService from "../services/response.service.js";
 const verifyToken = (req,res,next) => {
       Jwt.verify(req.headers.authorization ? req.headers.authorization.split(' ')[1] : '',process.env.JWT_SECRET,(err,result)=>{
             if(err){
-                return res.status(403).json({
-                    "message": "UnAuthorized"
-                })
+              return  ResponseService.sendResult(res,[],"UnAuthorized","error",403)
             }
             return next()
         }) 
